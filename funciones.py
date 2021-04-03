65bfa5accf9f97e56be36b42a68ca781ef615c4f
edualt <81114912+edualt@users.noreply.github.com>
2021-04-03 14:21:20 -0600

    Create funciones.py

---
+++ funciones.py
@@ -0,0 +1,22 @@
+import os
+import subprocess
+import ffmpeg
+import fnmatch, glob, traceback, errno, sys, atexit, locale, imp, stat
+from pytube import YouTube
+import pytube
+
+def mp4(link):
+    return YouTube(link).streams.first().download("/home/eduardo/Descargas/pytube")
+
+def mp3(link):
+    aud = link.streams.filter(only_audio=True).first() #Extraer solo el audio
+    destino = "/home/eduardo/Descargas/pytube"
+    
+    out_file = aud.download(output_path=destino)
+    
+    base, ext= os.path.splitext(out_file)
+    new_file = base + ".mp3"
+    os.rename(out_file, new_file)
+    
+    print(link.title + "Se descargo correctamente")
+
