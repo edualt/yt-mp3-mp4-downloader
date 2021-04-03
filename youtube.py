fc06ce3730d8a3eeb3e925ff0f4eb500e0fdb2f3
edualt <81114912+edualt@users.noreply.github.com>
2021-04-03 15:50:01 -0600

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -4,9 +4,15 @@ from pytube import YouTube
 print("Este es un sistema para descargar videos y musica, ingrese el link a continuacion")
 link = input()
 
+while True:
+    if "youtube.com/watch?v=" in link:
+        break
+    else:
+        print("hay algo mal, vuelva a ingresar el link!")
+    link = input()
+
 print("En que formato desea descargarlo? (mp3 o mp4)")
 vid = input()
-
 
 if vid == "mp4":
     video = funciones.mp4(link)
